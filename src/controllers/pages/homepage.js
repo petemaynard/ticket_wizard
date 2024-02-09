c30d5013647c8b5caea5926cd807e3a1fd2b752b
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-02-09 10:48:48 -0600

    commit for pull

--- src/controllers/pages/homepage.js
+++ src/controllers/pages/homepage.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+});
+
+
+
 module.exports = router;
