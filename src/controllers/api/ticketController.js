5a9e9c5efd9a9448351a996ae57ba938f8ddbe93
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-02-09 12:33:51 -0600

    commit for pull

--- src/controllers/api/ticketController.js
+++ src/controllers/api/ticketController.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
 const { Ticket } = require('../../../db/models/TicketPrice');
 
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 module.exports = router;
