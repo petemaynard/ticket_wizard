128fe9ffd3cff024275c9a31d4323ab5a1e4eb54
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-02-10 15:54:47 -0600

    added ticket and concert basic routes

--- src/controllers/api/concertController.js
+++ src/controllers/api/concertController.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
-const { Performance } = require('../../../db/models/Performance');
+const { Performance } = require('../../../db/models');
+var currentSearch
 
-router.get('/', async (req, res) => {
+router.get('/search', async (req, res) => {
     try {
+        const searchResults = await Performance.findAll({ where: { city: currentSearch } || { artist: currentSearch }});
+
+        res.json(searchResults);
 
     } catch (err) {
         res.status(400).json(err);
-    }
+    };
 });
 
 module.exports = router;
--- src/controllers/api/ticketController.js
+++ src/controllers/api/ticketController.js
@@ -1,12 +1,31 @@
 const router = require('express').Router();
-const { TicketPrice } = require('../../../db/models/TicketPrice');
+const { TicketPrices } = require('../../../db/models');
+const { Performance } = require('../../../db/models');
 
-router.get('/', async (req, res) => {
+router.get('/store', async (req, res) => {
     try {
+        const ticketData = await TicketPrices.findAll();
+        const multiplier = await Performance.findAll();
+        const ticketCost = ticketData.map(seat_base_price => seat_base_price * multiplier)
+
+        res.json(ticketCost);
 
     } catch (err) {
         res.status(400).json(err);
-    }
+    };
+});
+
+router.get('/checkout', async (req, res) => {
+    try {
+        const ticketData = await TicketPrices.findAll();
+        const multiplier = await Performance.findAll();
+        const ticketCost = ticketData.map(seat_base_price => seat_base_price * multiplier)
+
+        res.json(multiplier, ticketCost);
+
+    } catch (err) {
+        res.status(400).json(err);
+    };
 });
 
 module.exports = router;
