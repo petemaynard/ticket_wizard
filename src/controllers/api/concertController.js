6afefb1e14865292e138dd7e2bced0065c9a71a4
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-02-12 10:24:30 -0600

    started on search api call

--- src/controllers/api/concertController.js
+++ src/controllers/api/concertController.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const { Performance } = require('../../../db/models');
 var currentSearch
 
-router.get('/search', async (req, res) => {
-    try {
-        const searchResults = await Performance.findAll({ where: { city: currentSearch } || { artist: currentSearch }});
+// router.get('/search', async (req, res) => {
+//     try {
+//         const searchResults = await Performance.findAll({ where: { city: currentSearch } || { artist: currentSearch }});
 
-        res.json(searchResults);
+//         res.json(searchResults);
 
-    } catch (err) {
-        res.status(400).json(err);
-    };
-});
+//     } catch (err) {
+//         res.status(400).json(err);
+//     };
+// });
 
 module.exports = router;
