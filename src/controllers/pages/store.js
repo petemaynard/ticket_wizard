5cfdd581cf390d2bd4b3ccbdc32e4ff9d36690fb
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-02-12 14:31:06 -0600

    created file for store page handlebars

--- src/controllers/pages/store.js
+++ src/controllers/pages/store.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Artist, Venue, PerformanceDates, TicketPrices, TaxRate, Purchases } = require('../../../db/models');
 const search = searchObject;//searchObjest is a standin for the item the user picks form the search results page
-
+const seatGrade = 
 
 router.get('/', async (req, res) => {
     console.log('The store page route');
@@ -22,9 +22,9 @@ router.post('/', async (req, res) => {
     console.log('attempting to post initial Purchase data');
     try {
         Purchases.create({
-            cust_id: ,
-            perf_id: ,
-            seat_grade: input_field.value,
+            cust_id: session.user_id,
+            perf_id: searchObject.perf_id,
+            seat_grade: seatGrade,
             seat_count: input_field.value
         });
     } catch (error) {
