5fad3adbcfef9875e60d53f9c54f74469fe8ff81
petemaynard <xerxespete@gmail.com>
2024-02-14 14:49:48 -0600

    Adding concert header to store page

--- src/controllers/pages/store.js
+++ src/controllers/pages/store.js
@@ -1,15 +1,31 @@
 const router = require('express').Router();
 const { Artist, Venue, PerformanceDates, TicketPrices, TaxRate, Purchases } = require('../../../db/models');
-// const searchObject = {
-//     artist_name: 'death gary',
-//     perf_id: '1'
-//     };//searchObjest is a standin for the item the user picks form the search results page
 
 router.get('/', async (req, res) => {
     console.log('The store page route');
+
+
+
     try {
-        const searchData = req.query;
+        const searchData = req.query;  // Passed in concert ID and empty string (perf_date_id)
         const [concertId, value] = Object.entries(searchData)[0];
+        // Removing empty string
+
+        // This will be displayed on top of table: PD.event_date, A.Artist, V.venue_name
+        const headerData = await PerformanceDates.findByPk(concertId, {
+         include: [{ model: Artist }, { model: Venue }]
+         })
+         console.log("Performance artist " + headerData.artist.artist_name);
+         console.log("date: "+ headerData.event_date);
+         console.log("Venue is: " + headerData.venue.venue_name);
+         const eventDate = headerData.event_date;
+         const eventArtist = headerData.artist.artist_name;
+         const eventVenue = headerData.venue.venue_name
+       
+
+        // This goes within the table: TP.seat_base_price, TP.seat_name_description
+
+
         const concert = await PerformanceDates.findByPk(concertId);
         const basePrice = await TicketPrices.findAll();
         const basePriceSerial = basePrice.map((event) =>
@@ -29,7 +45,7 @@ router.get('/', async (req, res) => {
             { grade: basePriceSerial[3].seat_grade, seat: basePriceSerial[3].seat_grade_desc, price: finalPrice[3] }
         ];
 
-        res.render('store', { priceAndSeat, band });
+        res.render('store', { priceAndSeat, band, eventDate, eventArtist, eventVenue });
     } catch (err) {
         console.log("Query failed");
         res.status(500).json(err);
