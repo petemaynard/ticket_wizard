9e2251577c94c269566eba9af95741b14ed77902
j-preim <j.preimesberger@gmail.com>
2024-02-14 18:53:36 -0600

    reviewOrder route updated

--- src/controllers/pages/reviewOrder.js
+++ src/controllers/pages/reviewOrder.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Artist, Venue, PerformanceDates, TicketPrices, TaxRate, Purchases } = require('../../../db/models');
+const { Artist, Venue, PerformanceDates, TicketPrices, TaxRate, Purchases, Customer } = require('../../../db/models');
 
 router.get('/', async (req, res) => {
     console.log('going to review order')
@@ -11,5 +11,25 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/:cust_tix_id', async (req, res) => {
+    console.log('going to review order')
+    try {
+        const loggedInCustId = req.session.user_id;
+
+        const customerData = await Customer.findByPk(loggedInCustId);
+
+        const customerTaxRate = await TaxRate.findByPk(customerData.state);
+
+        console.log(customerData.state);
+        console.log(customerTaxRate.rate);
+
+        res.render('reviewOrder');
+    } catch (err) {
+        console.log("Query failed");
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
