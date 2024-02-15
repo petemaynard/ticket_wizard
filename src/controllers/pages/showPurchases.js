18fafa77441ad7199af6b1c51d5ba4896657a3b9
petemaynard <xerxespete@gmail.com>
2024-02-15 00:43:13 -0600

    Only purchased records displayed on purchases

--- src/controllers/pages/showPurchases.js
+++ src/controllers/pages/showPurchases.js
@@ -4,43 +4,45 @@ const { Op } = require("sequelize");
 const sequelize = require('../../config/connection');
 
 router.get("/", async (req, res) => {
-  try {
-   const loggedInCustId = req.session.user_id;
+   try {
+      const loggedInCustId = req.session.user_id;
 
-   const customerData = await Customer.findByPk(loggedInCustId, {
-      include: [{
-         model: Purchases,
-         include: [
-            { model: TicketPrices },
-            {
-               model: PerformanceDates,
-               include: [{ model: Artist }, { model: Venue }]
-            }
-         ]
-      }],
-   });
+      const customerData = await Customer.findByPk(loggedInCustId, {
+         include: [{
+            model: Purchases,
+            include: [
+               { model: TicketPrices },
+               {
+                  model: PerformanceDates,
+                  include: [{ model: Artist }, { model: Venue }]
+               }
+            ],
+            where: { purchased: 1 },
+         }],
+      });
 
-   const customer = customerData.get({ plain: true })
+      const customer = customerData.get({ plain: true })
 
-   res.render('showpurchases', { customer, logged_in: req.session.logged_in});
+      res.render('showpurchases', { customer, logged_in: req.session.logged_in });
 
-  } catch (err) {
-    console.log("Query failed");
-    res.status(500).json(err);
-  }
+   } catch (err) {
+      console.log("Query failed");
+      res.status(500).json(err);
+   }
 });
 
-      /*  The above sequelize query in simple SQL
-      USE wizard_db;            
-        SELECT PD.event_date, A.artist_name, V.venue_name, TP.seat_grade_desc, P.seat_count
-        FROM performancedates PD
-        INNER JOIN artist A ON PD.artist_id = A.artist_id
-        INNER JOIN purchases P ON PD.perf_date_id = P.perf_id
-        INNER JOIN venue V ON PD.venue_id = V.venue_id
-        INNER JOIN ticketprices TP ON P.seat_grade = TP.seat_grade
-        INNER JOIN customer C on P.cust_id = C.cust_id
-        WHERE P.cust_id = ?;
-      */
+/*  The above sequelize query in simple SQL
+USE wizard_db;            
+  SELECT PD.event_date, A.artist_name, V.venue_name, TP.seat_grade_desc, P.seat_count
+  FROM performancedates PD
+  INNER JOIN artist A ON PD.artist_id = A.artist_id
+  INNER JOIN purchases P ON PD.perf_date_id = P.perf_id
+  INNER JOIN venue V ON PD.venue_id = V.venue_id
+  INNER JOIN ticketprices TP ON P.seat_grade = TP.seat_grade
+  INNER JOIN customer C on P.cust_id = C.cust_id
+  WHERE P.cust_id = ?
+  and P.purchased = 1;
+*/
 
 
 module.exports = router;
