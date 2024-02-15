ad19d6ac44e561d5390bd9344777e8e81f96ad87
Dan Brandt <57990001+DB-Doo@users.noreply.github.com>
2024-02-14 22:45:46 -0600

    Update search.js

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -101,5 +101,5 @@ function createResultElement(item, type) {
 
 function formatDate(date) {
   const dateArray = date.split("-");
-    return ${new Number(dateArray[1])}/${new Number(dateArray[2])}/${new Number(dateArray[0])};
+    return `${new Number(dateArray[1])}/${new Number(dateArray[2])}/${new Number(dateArray[0])}`;
 }
